d0126ecd42f3c1e373737efa7bb58a7199a95b1b
Riley Nielson <rileyanielson@gmail.com>
2023-08-03 22:39:54 -0600

    Adding Login Authentication with google

--- src/components/3.Footer/footer.jsx
+++ src/components/3.Footer/footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div id="footer">
+      
       <img id="footerImg" src="./images/OlffloLogoWhite.png" alt="Olfflo" />
       <p>Copyright 2023</p>
       <img id="footerImg" src="./images/OlffloLogoWhite.png" alt="Olfflo" />
