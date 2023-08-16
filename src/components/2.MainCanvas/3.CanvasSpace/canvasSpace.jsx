bd0303c8091e455e70a59c9ca735692f6c299e38
Riley Nielson <rileyanielson@gmail.com>
2023-08-16 01:31:03 -0600

    Updating Canvas

--- src/components/2.MainCanvas/3.CanvasSpace/canvasSpace.jsx
+++ src/components/2.MainCanvas/3.CanvasSpace/canvasSpace.jsx
@@ -1,76 +1,60 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import zoomVar from "../../zoomVar";
+import { Stage, Layer, Rect, Text, Arrow } from "react-konva";
 // import { update } from "@react-spring/web";
 
 function CanvasSpace(props) {
-  const canvas = useRef();
-  let ctx = null;
+  // const [arrows, setArrows] = useState([]);
+  var arrows = [];
 
-  // initialize the canvas context
-  useEffect(() => {
-    // dynamically assign the width and height to canvas
-    const canvasEle = canvas.current;
-    canvasEle.width = canvasEle.clientWidth;
-    canvasEle.height = canvasEle.clientHeight;
+  console.log(props.itemList);
 
-    // get context of the canvas
-    ctx = canvasEle.getContext("2d");
+  props.itemList.map((item) => {
+    item.subItems.map((s) => {
+      drawFillRect(s);
+    });
+    drawFillRect(item);
   });
 
-  useEffect(() => {
-    props.itemList.map((item) => {
-      drawFillRect(item);
-    });
-    props.itemList.map((item) => {
-      item.feeds.map((suc, sucIndex) => {
-        suc !== null && drawArrow(item, suc, sucIndex);
-      });
+  props.itemList.map((item) => {
+    item.feeds.map((suc, sucIndex) => {
+      suc !== null &&
+        drawArrow(item, suc, sucIndex) &&
+        suc.subItems.feeds.map((suc2, suc2Index) => {
+          suc2 !== null &&
+            drawArrow(item, suc2, suc2Index) &&
+            suc2.subItems.feeds.map((suc3, suc3Index) => {
+              suc3 !== null && drawArrow(item, suc3, suc3Index);
+            });
+        });
     });
   });
 
   // draw rectangle with background
-  const drawFillRect = (info) => {
+  function drawFillRect(info) {
     const totalWidth = 5100 * zoomVar * 1.55;
-    const totalHeight = 3300 * zoomVar;
+    const totalHeight = 3300 * zoomVar * 1.55;
     const itemCount = props.itemList.length;
     const spacesCount = itemCount * 1.618 - 0.618;
-    const canvasBorderWidth = 400;
+    const canvasBorderWidth = 200;
     const spaceWidth = (totalWidth - canvasBorderWidth) / spacesCount;
 
-    const x = (info.id - 1) * spaceWidth * 1.618 + canvasBorderWidth / 2;
+    var x = (info.id - 1) * spaceWidth * 1.618 + canvasBorderWidth / 2;
     const w = spaceWidth;
     const h = 500 * zoomVar;
-    const y = totalHeight / 2 - h / 2;
+    var y = totalHeight / 2 - h / 2;
+
+    if (typeof info.id === "string" && info.id.includes(".")) {
+      y = y +100;
+    }
 
     info.coords = [x, y, w, h];
+  }
 
-    const textx = x + w / 2;
-    const texty = y + h / 2;
-
-    const borderColor = "rgb(0, 107, 166)",
-      borderWidth = "2px",
-      backgroundColor = "white";
-
-    ctx.beginPath();
-    ctx.fillStyle = backgroundColor;
-    ctx.strokeStyle = borderColor;
-    ctx.lineWidth = borderWidth;
-    ctx.rect(x, y, w, h);
-    ctx.fillRect(x, y, w, h);
-    ctx.stroke();
-
-    ctx.font = 90 * zoomVar + "px Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = "rgb(4, 150, 255)";
-    ctx.measureText(info.title); // TextMetrics object
-    ctx.fillText(info.title, textx, texty);
-  };
-
-  const drawArrow = (fromItem, toID, sucIndex) => {
+  function drawArrow(fromItem, toID, sucIndex) {
     const toItem = props.itemList[toID - 1];
     //variables to be used when creating the arrow
-    const totalHeight = 3300 * zoomVar;
+    const totalHeight = 3300 * zoomVar * 1.55;
 
     const x = fromItem.coords[0];
     const y = totalHeight / 2;
@@ -78,46 +62,42 @@ function CanvasSpace(props) {
 
     var fromx = x + w,
       fromy = y,
-      tox = toItem.coords[0] - 4,
-      toy = y,
-      tox1,
-      toy1,
-      tox2,
-      toy2;
+      tox = toItem.coords[0] - fromx - 4,
+      toy = 0,
+      tox1 = 0,
+      toy1 = 0,
+      tox2 = 0,
+      toy2 = 0;
     if (fromItem.id + 1 < toID) {
       fromx = x + (w * 3) / 4;
       fromy = y + fromItem.coords[3] / 2;
-      tox1 = fromx;
-      toy1 = y + 50 + 15 * (props.itemList.length - fromItem.id - 1);
-      tox2 = tox + toItem.coords[2] / 4 + 4;
+      tox1 = 0;
+      toy1 = 50 + 15 * (props.itemList.length - fromItem.id - 1);
+      tox2 = toItem.coords[0] + w / 3 - fromx;
       toy2 = toy1;
       tox = tox2;
-      toy = fromy + 4;
+      toy = 0 + 4;
     } else if (fromItem.id + 1 > toID) {
-      fromx = x + w / 4;
+      fromx = x + w / 3;
       fromy = y - fromItem.coords[3] / 2;
-      tox1 = fromx;
-      toy1 = y - 50 - 15 * (props.itemList.length - toID);
-      tox2 = tox + (toItem.coords[2] * 3) / 4 + 4;
+      tox1 = 0;
+      toy1 = 0 - 20 - 15 * (props.itemList.length - toID);
+      tox2 = toItem.coords[0] + (w * 2) / 3 - fromx;
       toy2 = toy1;
       tox = tox2;
-      toy = fromy - 4;
+      toy = 0 - 4;
     }
-    var headlen = 10;
-    var angle = Math.atan2(toy - fromy, tox - fromx);
 
-    ctx.save();
+    if (typeof toItem.id === "string" && toItem.id.includes(".")) {
+      y = y * 1.75;
+    }
 
-    //starting path of the arrow from the start square to the end square
-    //and drawing the stroke
-    ctx.beginPath();
+    var strokeStyle;
 
     if (fromItem.id + 1 === toID) {
-      ctx.strokeStyle = "rgb(255, 188, 66)";
-      ctx.moveTo(fromx, fromy);
-      ctx.lineTo(tox, toy);
+      strokeStyle = "rgb(255, 188, 66)";
     } else if (fromItem.id + 1 < toID) {
-      ctx.strokeStyle =
+      strokeStyle =
         "rgb(" +
         (4 +
           Math.floor((255 - 4) / (props.itemList.length - 2)) *
@@ -131,16 +111,11 @@ function CanvasSpace(props) {
           Math.floor((255 - 66) / (props.itemList.length - 2)) *
             (fromItem.id - 1)) +
         ")";
-      ctx.moveTo(fromx, fromy);
-      ctx.lineTo(tox1, toy1);
-      ctx.lineTo(tox2, toy2);
-      ctx.lineTo(tox, toy);
-      angle = Math.atan2(toy - toy2, tox - tox2);
     } else {
-      ctx.strokeStyle =
+      strokeStyle =
         "rgb(" +
         (216 +
-          Math.floor((255-216) / (props.itemList.length - 1)) * (toID - 1)) +
+          Math.floor((255 - 216) / (props.itemList.length - 1)) * (toID - 1)) +
         ", " +
         (17 +
           Math.floor((188 - 17) / (props.itemList.length - 1)) * (toID - 1)) +
@@ -148,51 +123,98 @@ function CanvasSpace(props) {
         (89 -
           Math.floor((89 - 66) / (props.itemList.length - 1)) * (toID - 1)) +
         ")";
-      ctx.moveTo(fromx, fromy);
-      ctx.lineTo(tox1, toy1);
-      ctx.lineTo(tox2, toy2);
-      ctx.lineTo(tox, toy);
-      angle = Math.atan2(toy - toy2, tox - tox2);
     }
-    ctx.lineWidth = 5;
-    ctx.stroke();
 
-    //starting a new path from the head of the arrow to one of the sides of
-    //the point
-    ctx.beginPath();
-    ctx.moveTo(tox, toy);
-    ctx.lineTo(
-      tox - headlen * Math.cos(angle - Math.PI / 7),
-      toy - headlen * Math.sin(angle - Math.PI / 7)
-    );
+    const arrowSpecs = {
+      x: fromx,
+      y: fromy,
+      points: [0, 0, tox1, toy1, tox2, toy2, tox, toy],
+      stroke: strokeStyle,
+    };
 
-    //path from the side point of the arrow, to the other side point
-    ctx.lineTo(
-      tox - headlen * Math.cos(angle + Math.PI / 7),
-      toy - headlen * Math.sin(angle + Math.PI / 7)
-    );
+    arrows = [...arrows, arrowSpecs];
+  }
 
-    //path from the side point back to the tip of the arrow, and then
-    //again to the opposite side point
-    ctx.lineTo(tox, toy);
-    ctx.lineTo(
-      tox - headlen * Math.cos(angle - Math.PI / 7),
-      toy - headlen * Math.sin(angle - Math.PI / 7)
-    );
+  console.log(arrows);
 
-    //draws the paths created above
-    ctx.stroke();
-    ctx.restore();
-  };
+  function handleOnEnter(item) {
+    console.log("Item is" + item);
+    // return item.subItems.map((s) => {
+    //   return (
+    //     <Rect
+    //       x={s.coords[0]}
+    //       y={s.coords[1]}
+    //       width={s.coords[2]}
+    //       height={s.coords[3]}
+    //       fill="white"
+    //       stroke="rgb(4, 150, 255)"
+    //       cornerRadius={10}
+    //     />
+    //   );
+    // });
+  }
 
   return (
     <div id="canvasSpace">
-      <canvas
+      <Stage
         id="canvas"
-        ref={canvas}
+        ref={props.canvasRef}
         width={5100 * zoomVar * 1.55}
-        height={3300 * zoomVar}
-      ></canvas>
+        height={3300 * zoomVar * 1.55}
+      >
+        <Layer>
+          {props.itemList.map((item) => (
+            <Rect
+              x={item.coords[0]}
+              y={item.coords[1]}
+              width={item.coords[2]}
+              height={item.coords[3]}
+              fill="white"
+              stroke="rgb(4, 150, 255)"
+              cornerRadius={10}
+            />
+          ))}
+          {props.itemList.map((item) => (
+            <Text
+              x={item.coords[0]}
+              y={item.coords[1]}
+              width={item.coords[2]}
+              height={item.coords[3]}
+              fontSize={20}
+              fill="rgb(4, 150, 255)"
+              text={item.title}
+              verticalAlign="middle"
+              align="center"
+              onMouseEnter={() => {
+                item.subItems.map((s) => {
+                  return (
+                    <Rect
+                      x={s.coords[0]}
+                      y={s.coords[1]}
+                      width={s.coords[2]}
+                      height={s.coords[3]}
+                      fill="black"
+                      stroke="rgb(4, 150, 255)"
+                      cornerRadius={10}
+                    />
+                  );
+                });
+              }}
+            />
+          ))}
+          {arrows.map((arr) => (
+            <Arrow
+              x={arr.x}
+              y={arr.y}
+              points={arr.points}
+              pointerLength={10}
+              pointerWidth={10}
+              stroke={arr.stroke}
+              strokeWidth={4}
+            />
+          ))}
+        </Layer>
+      </Stage>
     </div>
   );
 }
